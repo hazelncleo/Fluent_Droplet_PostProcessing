94159ac0970548f411cc90f2c98db21f9d7349ab
Hazel Full <hazelncleo@gmail.com>
2026-03-09 16:20:10 +1100

    Added 1000nm_noisy_fullduty data, updated plotter to rolling average the shearrates.

--- plotter.py
+++ plotter.py
@@ -11,7 +11,9 @@ files = [
     'output/750nm.csv',
     'output/1000nm.csv',
     'output/500nm_noisy.csv',
-    'output/1000nm_noisy.csv'
+    'output/1000nm_noisy.csv',
+    'output/1000nm_fullduty.csv',
+    'output/1000nm_noisy_fullduty.csv'
 ]
 
 data = {
@@ -20,7 +22,9 @@ data = {
     '750nm' : pd.read_csv(files[1]),
     '1000nm' : pd.read_csv(files[2]),
     '500nm_noisy' : pd.read_csv(files[3]),
-    '1000nm_noisy' : pd.read_csv(files[4])
+    '1000nm_noisy' : pd.read_csv(files[4]),
+    '1000nm_fullduty' : pd.read_csv(files[5]),
+    '1000nm_noisy_fullduty' : pd.read_csv(files[6])
 }
 
 
@@ -28,13 +32,14 @@ f,(ax_a,ax_n) = plt.subplots(2,3, figsize=(18,12))
 f.tight_layout(pad=5)
 
 #ax_a[0].plot(data['250nm']['cycle_time'],data['250nm']['max_shearrate'],'-r')
-ax_a[0].plot(data['500nm']['cycle_time'],data['500nm']['max_shearrate'],'-g')
-ax_a[0].plot(data['750nm']['cycle_time'],data['750nm']['max_shearrate'],'-b')
-ax_a[0].plot(data['1000nm']['cycle_time'],data['1000nm']['max_shearrate'],'-k')
+ax_a[0].plot(data['500nm']['cycle_time'],data['500nm']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-g')
+ax_a[0].plot(data['750nm']['cycle_time'],data['750nm']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-b')
+ax_a[0].plot(data['1000nm']['cycle_time'],data['1000nm']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-k')
+ax_a[0].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-c')
 ax_a[0].set_yscale('log')
 ax_a[0].set(
     title = 'Max Shearrate for different amplitudes',
-    xlim = [-0.1,60],
+    xlim = [-0.1,200],
     ylim = [1e4,1e8],
     xlabel = 'N Cycles',
     ylabel = r'Max Shearrate $(\frac{1}{s})$'
@@ -44,10 +49,11 @@ ax_a[0].set(
 ax_a[1].plot(data['500nm']['cycle_time'],data['500nm']['total_volume_delivered'],'-g')
 ax_a[1].plot(data['750nm']['cycle_time'],data['750nm']['total_volume_delivered'],'-b')
 ax_a[1].plot(data['1000nm']['cycle_time'],data['1000nm']['total_volume_delivered'],'-k')
+ax_a[1].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['total_volume_delivered'],'-c')
 ax_a[1].set(
     title = 'Total volume delivered for different amplitudes',
-    xlim = [-0.1,60],
-    ylim = [-0.00001,0.00025],
+    xlim = [-0.1,200],
+    ylim = [-0.00001,0.001],
     xlabel = 'N Cycles',
     ylabel = r'Total Volume Delivered $(\mu L)$'
 )
@@ -56,25 +62,29 @@ ax_a[1].set(
 ax_a[2].plot(data['500nm']['cycle_time'],data['500nm']['volumetric_flowrate'],'-g', label='500nm')
 ax_a[2].plot(data['750nm']['cycle_time'],data['750nm']['volumetric_flowrate'],'-b', label='750nm')
 ax_a[2].plot(data['1000nm']['cycle_time'],data['1000nm']['volumetric_flowrate'],'-k', label='1000nm')
+ax_a[2].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['volumetric_flowrate'],'-c', label='1000nm full dutycycle')
 ax_a[2].plot([-500,-500],[-250,-250],'-y', label='500nm noisy')
 ax_a[2].plot([-500,-500],[-250,-250],'-r', label='1000nm noisy')
+ax_a[2].plot([-500,-500],[-250,-250],'-m', label='1000nm noisy fullduty')
 ax_a[2].set(
     title = 'Volumetric Flowrate for different amplitudes',
-    xlim = [-0.1,60],
-    ylim = [-0.1,7],
+    xlim = [-0.1,200],
+    ylim = [-0.1,8],
     xlabel = 'N Cycles',
     ylabel = r'Volumetric Flowrate $(\frac{\mu L}{s})$'
 )
-ax_a[2].legend(loc='center right', bbox_to_anchor=(0.4, 0.75), fancybox=True, shadow=True)
+ax_a[2].legend(loc='lower right', fancybox=True, shadow=True)
 
-ax_n[0].plot(data['500nm']['cycle_time'],data['500nm']['max_shearrate'],'-g')
-ax_n[0].plot(data['1000nm']['cycle_time'],data['1000nm']['max_shearrate'],'-k')
-ax_n[0].plot(data['500nm_noisy']['cycle_time'],data['500nm_noisy']['max_shearrate'],'-y')
-ax_n[0].plot(data['1000nm_noisy']['cycle_time'],data['1000nm_noisy']['max_shearrate'],'-r')
+ax_n[0].plot(data['500nm']['cycle_time'],data['500nm']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-g')
+ax_n[0].plot(data['1000nm']['cycle_time'],data['1000nm']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-k')
+ax_n[0].plot(data['500nm_noisy']['cycle_time'],data['500nm_noisy']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-y')
+ax_n[0].plot(data['1000nm_noisy']['cycle_time'],data['1000nm_noisy']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-r')
+ax_n[0].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-c')
+ax_n[0].plot(data['1000nm_noisy_fullduty']['cycle_time'],data['1000nm_noisy_fullduty']['max_shearrate'].rolling(window=15,min_periods=1,center=True).mean(),'-m')
 ax_n[0].set_yscale('log')
 ax_n[0].set(
     title = 'Max shearrate with and without noise',
-    xlim = [-0.1,60],
+    xlim = [-0.1,200],
     ylim = [1e4,1e8],
     xlabel = 'N Cycles',
     ylabel = r'Max Shearrate $(\frac{1}{s})$'
@@ -83,20 +93,24 @@ ax_n[0].set(
 
 ax_n[1].plot(data['1000nm']['cycle_time'],data['1000nm']['total_volume_delivered'],'-k')
 ax_n[1].plot(data['1000nm_noisy']['cycle_time'],data['1000nm_noisy']['total_volume_delivered'],'-r')
+ax_n[1].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['total_volume_delivered'],'-c')
+ax_n[1].plot(data['1000nm_noisy_fullduty']['cycle_time'],data['1000nm_noisy_fullduty']['total_volume_delivered'],'-m')
 ax_n[1].set(
     title = 'total volume delivered with and without noise',
-    xlim = [-0.1,60],
-    ylim = [-0.00001,0.00025],
+    xlim = [-0.1,200],
+    ylim = [-0.00001,0.001],
     xlabel = 'N Cycles',
     ylabel = r'Total Volume Delivered $(\mu L)$'
 )
 
 ax_n[2].plot(data['1000nm']['cycle_time'],data['1000nm']['volumetric_flowrate'],'-k')
 ax_n[2].plot(data['1000nm_noisy']['cycle_time'],data['1000nm_noisy']['volumetric_flowrate'],'-r')
+ax_n[2].plot(data['1000nm_fullduty']['cycle_time'],data['1000nm_fullduty']['volumetric_flowrate'],'-c')
+ax_n[2].plot(data['1000nm_noisy_fullduty']['cycle_time'],data['1000nm_noisy_fullduty']['volumetric_flowrate'],'-m')
 ax_n[2].set(
     title = 'Volumetric flowrate with and without noise',
-    xlim = [0,60],
-    ylim = [-0.1,7],
+    xlim = [0,200],
+    ylim = [-0.1,8],
     xlabel = 'N Cycles',
     ylabel = r'Volumetric Flowrate $(\frac{\mu L}{s})$'
 )
