8f3c2df2927206705407edfee14f35c59d8a6e1a
hazel full <hazelncleo@gmail.com>
2026-04-01 09:33:15 +1100

    Minor changes to droplet plotting.

--- droplet_sizing_script/droplets.py
+++ droplet_sizing_script/droplets.py
@@ -9,12 +9,17 @@ volume = df['volume'][1:]*1e18
 ux = df['ux'][1:]
 uy = df['uy'][1:]
 uz = df['uz'][1:]
+vx = df['vx'][1:]
+vy = df['vy'][1:]
+vz = df['vz'][1:]
+velocity = np.sqrt(vx**2 + vy**2 + vz**2)
 diameter = 2 * np.power((3 / (4 * np.pi)) * volume, 1/3)
 
-f,ax = plt.subplots(2,3, figsize = (15,6))
-ax[0,0].hist(volume*1e-9)
-ax[0,1].hist(diameter)
-ax[0,2].scatter(ux,uy,s=diameter,c='k')
-ax[1,0].scatter(ux,uz,s=diameter,c='k')
+f,ax = plt.subplots(1,4, figsize = (14,4))
+ax[0].hist(diameter,bins=15)
+ax[1].hist(diameter,weights=volume,bins=15)
+ax[2].hist(velocity,bins=15)
+ax[3].hist(velocity[uz < 500e-6])
+
 
 f.savefig('test.png', dpi=750)
