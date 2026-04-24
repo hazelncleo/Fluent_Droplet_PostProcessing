acdc0a09e719783c93d9e158409b8be2dfa44b10
Hazel Full <hazelncleo@gmail.com>
2026-04-24 15:57:54 +1000

    Started working on droplet visualizations

--- droplet_sizing_script_controller.py
+++ droplet_sizing_script_controller.py
@@ -1,3 +1,20 @@
+import pandas as pd
+import glob
+import os
+import re
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
 
 class DropletSizingScriptController:
     def __init__(self, parameters, folder):
@@ -10,7 +27,15 @@ class DropletSizingScriptController:
 
         :param self: Description
         '''
-        self.droplets_calculated = True
+
+        csv_files = glob.glob(os.path.join(self.folder, 'droplets_data', '*.csv'))
+        csv_files.sort(key=alphanum_key)
+
+        for droplet_file in csv_files:
+            droplet_data = pd.read_csv(droplet_file, skiprows=8)
+            print('bruh')
+
+
 
 
     def droplet_sizing_plot(self): # TODO
@@ -20,3 +45,9 @@ class DropletSizingScriptController:
     def droplet_sizing_animation(self): # TODO
         pass
 
+
+
+if __name__ == '__main__':
+    sizer = DropletSizingScriptController(None, 'test_datasets/droplets')
+
+    sizer.droplet_sizing_calculation()
